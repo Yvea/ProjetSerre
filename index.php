7ab6e3a6847324db7464361f8bd9b21e8b6ac7da
Q-LUCAS <qlucas@la-providence.net>
2020-01-20 15:19:20 +0100

    Création du squelette du formulaire

--- index.php
+++ index.php
@@ -5,3 +5,22 @@
 <!-- PAGE INDEX.PHP : ACCUEIL / FORMULAIRE DE CONNEXION -->
 <!-------------------------------------------------------->
 <!-------------------------------------------------------->
+
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="styleshet" href="style.css"/>
+        <link rel="icon" href="" />
+        <title>SERRE ~ CONNEXION</title>
+    </head>
+
+    <body>
+        <div class="FormCo">
+            <form method="POST" action="action.php">
+                <input type="text" name="login" placeholder="IDENTIFIANT"/>
+                <input type="password" name="mdp" placeholder="MOT DE PASSE"/>
+                <input type="submit" name="co" value="SE CONNECTER"/>
+            </form>
+        </div>
+    </body>
+</html>
