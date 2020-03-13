bc996b68c4fa53ba02104ef4ea94f0560f809b3e
BeaZOWIE <49244338+BeaZOWIE@users.noreply.github.com>
2020-03-13 10:29:57 +0100

    node

--- GestionSerre.js
+++ GestionSerre.js
@@ -1,3 +1,22 @@
-class GestionSerre{
+const Capteur = require("./Capteur");
+const Actionneur = require("./Actionneur");
+
+module.exports = class GestionSerre {
+
+    constructor()
+    {
+        this.captTempInt = new Capteur('tempInt');
+
+        this.act
+    }
+
+    TestValeurs()
+    {
+        var tempInt = this.capteurTempInt.GetValue('tempInt')
+        if(tempInt > 25)
+        {
+
+        }
+    }
     
 }
