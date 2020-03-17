5fa8ad572ae63c30491f4547304926d644c48aa4
BeaZOWIE <49244338+BeaZOWIE@users.noreply.github.com>
2020-03-17 17:43:49 +0100

    le capteur oui

--- Capteur.js
+++ Capteur.js
@@ -1,26 +1,9 @@
 const fetch = require("../node_modules/node-fetch");
 
 module.exports = class Capteur {
-    
-    
-    //json.Monitor.S.S1.item1.value
-    /*constructor(Jsonpath1,Jsonpath2,Jsonpath3)//constructeur HumiditéSol
-    {
-        this.value1 = Jsonpath1
-        this.value2 = Jsonpath2
-        this.value3 = Jsonpath3
-        var moyen = this.MoyenneHumidSol();
-    }*/
-
-    constructor(IdCapteur)//constructeur general
+    constructor(IdCapteur)
     {
         this.ID = IdCapteur;
-
-        if(this.ID == 'humidSol')
-        {
-            this.humid = this.MoyenneHumidSol(callback);
-        }
-
         this.GetValue();
     }
 
@@ -33,42 +16,30 @@ module.exports = class Capteur {
             switch (that.ID)
             {
                 case 'tempInt':
-                    that.valeur = json.Monitor.S.S1.item1.value;
+                    that.valeur = parseFloat(json.Monitor.S.S1.item1.value);
                     break;
                 case 'tempExt':
-                    that.valeur = json.Monitor.S.S1.item1.value;
+                    that.valeur = parseFloat(json.Monitor.S.S1.item1.value);
                     break;
                 case 'humidAir':
-                    that.valeur = json.Monitor.S.S1.item2.value;
+                    that.valeur = parseFloat(json.Monitor.S.S1.item2.value);
                     break;
-                case 'humidSol1':
-                    that.valeur = json.Monitor.S.S1.item1.value;
-                    break;
-                case 'humidSol2':
-                    that.valeur = json.Monitor.S.S1.item1.value;
-                    break;
-                case 'humidSol3':
-                    that.valeur = json.Monitor.S.S1.item1.value;
+                case 'humidSol':
+                    let valeur1 = parseFloat(json.Monitor.S.S1.item1.value);
+                    let valeur2 = parseFloat(json.Monitor.S.S1.item1.value);
+                    let valeur3 = parseFloat(json.Monitor.S.S1.item1.value);
+                    let valeurM = valeur1 + valeur2 + valeur3;
+                    that.valeur = valeurM / 3;
                     break;
             }
         }
         
-        fetch('http://192.168.64.200/status.json?a=admin:admin', {
+        fetch('http://192.168.0.200/status.json?a=admin:admin', {
             method: 'GET'
         })
         .then(reponse => reponse.json())
         .then(json => data(json));
 
         return this.valeur;
-    }
-
-    MoyenneHumidSol(callback)
-    {
-        let CapteurH1 = this.GetValue('humidSol1',callback);
-        let CapteurH2 = this.GetValue('humidSol2',callback);
-        let CapteurH3 = this.GetValue('humidSol3',callback);
-        var valueTot = 1 + 2 + 3;
-        var valueMoy = valueTot / 3;
-        return valueMoy;
     }
 }
